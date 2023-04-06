e61886fae06229c5895c8242ef9dd5d3bd4f6055
Ivaskaa <ivansha200@gmail.com>
2023-04-06 12:44:14 +0300

    project architecture

--- src/main/java/com/example/TradeBot/controller/InventoryController.java
+++ src/main/java/com/example/TradeBot/controller/InventoryController.java
@@ -15,14 +15,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
-import ua.avada.dentalcolouradminpanel.dto.country.CountryResponse;
-import ua.avada.dentalcolouradminpanel.dto.statistic.request.StatisticsChartRequest;
-import ua.avada.dentalcolouradminpanel.dto.statistic.response.*;
-import ua.avada.dentalcolouradminpanel.service.statistic.StatisticService;
 
 import javax.validation.Valid;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Controller
