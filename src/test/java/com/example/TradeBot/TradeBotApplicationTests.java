2e45f606f675bbea8f112db995eb6816add076dc
Ivaskaa <ivansha200@gmail.com>
2023-04-06 11:48:49 +0300

    Initial commit

---
+++ src/test/java/com/example/TradeBot/TradeBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TradeBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
