c9bc49bfce62f2f9a1bc5d6a947df64575d336fd
McSapoh <bodyamelnyk04@gmail.com>
2023-04-19 11:21:49 +0300

    Created WeaponTag dto with List of weapon tags

---
+++ src/main/java/com/example/TradeBot/dto/WeaponTag.java
@@ -0,0 +1,13 @@
+package com.example.TradeBot.dto;
+
+import java.util.AbstractList;
+import java.util.List;
+
+public class WeaponTag {
+    public static final List<String> tags = List.of("aug","awp","cz75a","deagle","elite", // beretas
+            "famas","fiveseven","g3sg1","galilar","glock","m4a1_silencer", "m4a1",
+            "mac10","mag7","mp5sd","mp7","mp9","negev","nova","hkp2000", // p2000
+            "p250","p90","bizon","revolver","sawedoff","scar20","sg556","ssg08","tec9",
+            "ump45","usp_silencer","xm1014"
+    );
+}
